32f2d5de62bed7645681ae359348570ac6e55e59
kerbukar <kerimeburcu@gmail.com>
2022-01-04 00:53:32 +0300

    Data katmanı oluşturuldu.

---
+++ KerimeBurcuKaratas_Odev2/API/Controllers/VehicleController.cs
@@ -0,0 +1,30 @@
+﻿using Data.Uow;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{  
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<VehicleController> _logger;
+        public VehicleController(IUnitOfWork unitOfWork,ILogger<VehicleController> logger)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+
+        }
+        
+        //Tüm araç listesini çekmek için kullanıldı.
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _unitOfWork.Vehicle.GetAll();
+            return Ok(result);  
+        }
+    }
+}
